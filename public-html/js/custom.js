1f6848c3458519e8349c81e9c06f32d3955077aa
isurunix <isurunix@gmail.com>
2017-02-08 14:28:09 +0530

    Added JQuery UI date picker to index page

---
+++ public-html/js/custom.js
@@ -0,0 +1,10 @@
+/**
+ * Created by isuru on 2/8/17.
+ */
+$( function() {
+    $( ".datepicker" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'yy-mm-dd'
+    });
+} );
